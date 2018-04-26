1bb82ef735016aa84475c8336689dc7ca324dada
Melissa Gilman <melissadgilman@yahoo.com>
2018-04-26 16:39:49 -0600

    npm install mySQL and inquirer

--- bamazon.js
+++ bamazon.js
@@ -1,18 +1,29 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
+var inquirer = require('inquirer');
 
-
+//mysql db connection
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon"
 });
- 
-connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    createTable();
 });
- 
+
+function createTable () {
+    connection.query("SELECT * FROM products", function (err, res){
+        for (var i=0; i<res.length; i++) {
+            console.log(res[i].itemid+" || " + res[i].productname + " || " + res[i].departmentname + " || " + res[i].price + " || " + res[i].stockquantity + "\n");
+            
+        }
+        
+    })
+};
+
+
 connection.end();
